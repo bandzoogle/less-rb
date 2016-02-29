7ad400f013273cb9331e9a7f51e33b2941bbab1e
Dave Spurr <david.spurr@gmail.com>
2016-02-29 17:11:49 +0000

    Run less.js test suite to test all less features
    This required us to include some other node modules that node-less depends on (image-size & mime)

--- lib/less.rb
+++ lib/less.rb
@@ -11,11 +11,15 @@ module Less
     Parser.new(options).parse(css).to_css
   end
 
-  def self.bundled_path
-    File.expand_path('../less/js', __FILE__)
+  def self.lib_paths
+    [
+      File.expand_path('../less/js/less/lib', __FILE__),
+      File.expand_path('../less/js/node-mime', __FILE__),
+      File.expand_path('../less/js', __FILE__)
+    ]
   end
 
   # Exports the `.node_modules` folder on the working directory so npm can
   # require modules installed locally.
-  ENV['NODE_PATH'] = "#{File.expand_path('node_modules')}:#{bundled_path}:#{ENV['NODE_PATH']}"
+  ENV['NODE_PATH'] = "#{File.expand_path('node_modules')}:#{lib_paths.join(':')}:#{ENV['NODE_PATH']}"
 end
--- spec/less/parser_spec.rb
+++ spec/less/parser_spec.rb
@@ -11,7 +11,7 @@ describe Less::Parser do
     end
 
     it "accepts options when parsing" do
-      subject.parse(".class {width: 1px+1px;}", :compress => true).to_css.strip.should eql ".class{width:2px}"
+      subject.parse(".class {width: 1px+1px;}", compress: true).to_css.strip.should eql ".class{width:2px}"
     end
 
     it "passes exceptions from the less compiler" do
@@ -45,7 +45,7 @@ describe Less::Parser do
   end
 
   describe "when configured with multiple load paths" do
-    subject { Less::Parser.new :paths => [ cwd.join('one'), cwd.join('two'), cwd.join('faulty') ] }
+    subject { Less::Parser.new paths: [ cwd.join('one'), cwd.join('two'), cwd.join('faulty') ] }
 
     it "will load files from both paths" do
       subject.parse('@import "one.less";').to_css.gsub(/\n/,'').strip.should eql ".one {  width: 1;}"
@@ -83,7 +83,7 @@ describe Less::Parser do
   end
 
   describe "when configured with multiple load paths" do
-    let(:parser) { Less::Parser.new(:paths => [cwd.join('one').to_s, cwd.join('two').to_s]) }
+    let(:parser) { Less::Parser.new(paths: [cwd.join('one').to_s, cwd.join('two').to_s]) }
 
     it "will load files from both paths" do
       parser.parse('@import "one.less";').to_css.gsub(/\n/,'').strip.should eql ".one {  width: 1;}"
@@ -105,6 +105,57 @@ describe Less::Parser do
       parser.parse('@import "one.less";').to_css.gsub(/\n/,'').strip.should eql ".one {  width: 1;}"
       parser.parse('@import "two.less";').to_css.gsub(/\n/,'').strip.should eql ".two {  width: 1;}"
     end
+  end
+
+  # NOTE: runs JS tests from less.js it's a replacement for less-test.js
+  describe "less.js test suite", :integration => true do
+
+    TEST_LESS_DIR = File.expand_path('../../lib/less/js/less/test/less', File.dirname(__FILE__))
+    TEST_CSS_DIR =  File.expand_path('../../lib/less/js/less/test/css' , File.dirname(__FILE__))
+
+    # before :all do
+    #   # functions.less test expects these exposed, see runner-main-options.js
+    #   Less.less.functions.functionRegistry.addMultiple({
+    #     add: -> (less, a, b) {
+    #       Less.tree['Dimension'].new(a['value'] + b['value'])
+    #     },
+
+    #     increment: -> (less, a) {
+    #       Less.tree['Dimension'].new(a['value'] + 1)
+    #     },
+
+    #     _color: -> (less, str) {
+    #       if str.value == 'evil red'
+    #         Less.tree['Color'].new('600')
+    #       end
+    #     }
+    #   })
+    # end
+
+    Dir.glob(File.join(TEST_LESS_DIR, '*.less')).each do |less_file|
+      base_name = File.basename(less_file, '.less')
+      css_file = File.join(TEST_CSS_DIR, "#{base_name}.css")
+      raise "missing css file: #{css_file}" unless File.exists?(css_file)
+
+      less_content = File.read(less_file)
+      case base_name
+        when 'javascript'
+          # adjust less .eval line :
+          #   title: `process.title`;
+          # later replaced by line :
+          #   title: `typeof process.title`;
+          # with something that won't fail (since we're not in Node.JS)
+          less_content.sub!('process.title', '"node"')
+      end
+
+      it "#{base_name}.less" do
+        parser = Less::Parser.new(filename: less_file, paths: [ File.dirname(less_file) ])
+        less = parser.parse( less_content, strictMath: true, silent: true, relativeUrls: true )
+        less.to_css.should == File.read(css_file)
+      end
+
+    end
+
   end
 
 end
